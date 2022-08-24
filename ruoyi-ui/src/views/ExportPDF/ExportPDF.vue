71e1bfd13daba27d997e15cd0885651e06ba1473
wangfuen <w1083991156@qq.com>
2022-08-24 17:10:13 +0800

    pdf发送并请求数据  8.24-2

--- ruoyi-ui/src/views/ExportPDF/ExportPDF.vue
+++ ruoyi-ui/src/views/ExportPDF/ExportPDF.vue
@@ -1025,9 +1025,7 @@ export default {
     get(){
       const loading = this.$loading({
         lock: true,//lock的修改符--默认是false
-        text: '正在分析数据，请稍后...\n' +
-              '这个过程可能需要1-3分钟的时间\n' +
-              '请勿刷新页面并耐心等待',//显示在加载图标下方的加载文案
+        text: '请勿刷新页面，正在获取数据，请耐心等待1-3分钟...',//显示在加载图标下方的加载文案
         spinner: 'el-icon-loading',//自定义加载图标类名
         background: 'rgba(0, 0, 0, 0.7)',//遮罩层颜色
         target: document.querySelector('#table')//loadin覆盖的dom元素节点
