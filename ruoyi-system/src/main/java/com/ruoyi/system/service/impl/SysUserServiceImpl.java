404772291e7c485c9b21dad34113c0276edcd723
hanhan <2668790143@qq.com>
2022-09-14 11:01:21 +0800

    Revert "登录/注册接口"
    This reverts commit 66d6288e7b1286bce08561e526afef7f3a522cf0.

--- ruoyi-system/src/main/java/com/ruoyi/system/service/impl/SysUserServiceImpl.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/service/impl/SysUserServiceImpl.java
@@ -32,7 +32,7 @@ import com.ruoyi.system.service.ISysUserService;
 
 /**
  * 用户 业务层处理
- *
+ * 
  * @author ruoyi
  */
 @Service
@@ -63,7 +63,7 @@ public class SysUserServiceImpl implements ISysUserService
 
     /**
      * 根据条件分页查询用户列表
-     *
+     * 
      * @param user 用户信息
      * @return 用户信息集合信息
      */
@@ -76,7 +76,7 @@ public class SysUserServiceImpl implements ISysUserService
 
     /**
      * 根据条件分页查询已分配用户角色列表
-     *
+     * 
      * @param user 用户信息
      * @return 用户信息集合信息
      */
@@ -89,7 +89,7 @@ public class SysUserServiceImpl implements ISysUserService
 
     /**
      * 根据条件分页查询未分配用户角色列表
-     *
+     * 
      * @param user 用户信息
      * @return 用户信息集合信息
      */
@@ -102,7 +102,7 @@ public class SysUserServiceImpl implements ISysUserService
 
     /**
      * 通过用户名查询用户
-     *
+     * 
      * @param userName 用户名
      * @return 用户对象信息
      */
@@ -114,7 +114,7 @@ public class SysUserServiceImpl implements ISysUserService
 
     /**
      * 通过用户ID查询用户
-     *
+     * 
      * @param userId 用户ID
      * @return 用户对象信息
      */
@@ -126,7 +126,7 @@ public class SysUserServiceImpl implements ISysUserService
 
     /**
      * 查询用户所属角色组
-     *
+     * 
      * @param userName 用户名
      * @return 结果
      */
@@ -143,7 +143,7 @@ public class SysUserServiceImpl implements ISysUserService
 
     /**
      * 查询用户所属岗位组
-     *
+     * 
      * @param userName 用户名
      * @return 结果
      */
@@ -160,7 +160,7 @@ public class SysUserServiceImpl implements ISysUserService
 
     /**
      * 校验用户名称是否唯一
-     *
+     * 
      * @param userName 用户名称
      * @return 结果
      */
@@ -213,7 +213,7 @@ public class SysUserServiceImpl implements ISysUserService
 
     /**
      * 校验用户是否允许操作
-     *
+     * 
      * @param user 用户信息
      */
     @Override
@@ -227,7 +227,7 @@ public class SysUserServiceImpl implements ISysUserService
 
     /**
      * 校验用户是否有数据权限
-     *
+     * 
      * @param userId 用户id
      */
     @Override
@@ -247,7 +247,7 @@ public class SysUserServiceImpl implements ISysUserService
 
     /**
      * 新增保存用户信息
-     *
+     * 
      * @param user 用户信息
      * @return 结果
      */
@@ -266,7 +266,7 @@ public class SysUserServiceImpl implements ISysUserService
 
     /**
      * 注册用户信息
-     *
+     * 
      * @param user 用户信息
      * @return 结果
      */
@@ -278,7 +278,7 @@ public class SysUserServiceImpl implements ISysUserService
 
     /**
      * 修改保存用户信息
-     *
+     * 
      * @param user 用户信息
      * @return 结果
      */
@@ -300,7 +300,7 @@ public class SysUserServiceImpl implements ISysUserService
 
     /**
      * 用户授权角色
-     *
+     * 
      * @param userId 用户ID
      * @param roleIds 角色组
      */
@@ -314,7 +314,7 @@ public class SysUserServiceImpl implements ISysUserService
 
     /**
      * 修改用户状态
-     *
+     * 
      * @param user 用户信息
      * @return 结果
      */
@@ -326,7 +326,7 @@ public class SysUserServiceImpl implements ISysUserService
 
     /**
      * 修改用户基本信息
-     *
+     * 
      * @param user 用户信息
      * @return 结果
      */
@@ -338,7 +338,7 @@ public class SysUserServiceImpl implements ISysUserService
 
     /**
      * 修改用户头像
-     *
+     * 
      * @param userName 用户名
      * @param avatar 头像地址
      * @return 结果
@@ -351,7 +351,7 @@ public class SysUserServiceImpl implements ISysUserService
 
     /**
      * 重置用户密码
-     *
+     * 
      * @param user 用户信息
      * @return 结果
      */
@@ -363,7 +363,7 @@ public class SysUserServiceImpl implements ISysUserService
 
     /**
      * 重置用户密码
-     *
+     * 
      * @param userName 用户名
      * @param password 密码
      * @return 结果
@@ -376,7 +376,7 @@ public class SysUserServiceImpl implements ISysUserService
 
     /**
      * 新增用户角色信息
-     *
+     * 
      * @param user 用户对象
      */
     public void insertUserRole(SysUser user)
@@ -386,7 +386,7 @@ public class SysUserServiceImpl implements ISysUserService
 
     /**
      * 新增用户岗位信息
-     *
+     * 
      * @param user 用户对象
      */
     public void insertUserPost(SysUser user)
@@ -409,7 +409,7 @@ public class SysUserServiceImpl implements ISysUserService
 
     /**
      * 新增用户角色信息
-     *
+     * 
      * @param userId 用户ID
      * @param roleIds 角色组
      */
@@ -432,7 +432,7 @@ public class SysUserServiceImpl implements ISysUserService
 
     /**
      * 通过用户ID删除用户
-     *
+     * 
      * @param userId 用户ID
      * @return 结果
      */
@@ -449,7 +449,7 @@ public class SysUserServiceImpl implements ISysUserService
 
     /**
      * 批量删除用户信息
-     *
+     * 
      * @param userIds 需要删除的用户ID
      * @return 结果
      */
@@ -471,7 +471,7 @@ public class SysUserServiceImpl implements ISysUserService
 
     /**
      * 导入用户数据
-     *
+     * 
      * @param userList 用户数据列表
      * @param isUpdateSupport 是否更新支持，如果已存在，则进行更新数据
      * @param operName 操作用户
@@ -479,7 +479,6 @@ public class SysUserServiceImpl implements ISysUserService
      */
     @Override
     public String importUser(List<SysUser> userList, Boolean isUpdateSupport, String operName)
-
     {
         if (StringUtils.isNull(userList) || userList.size() == 0)
         {
@@ -537,16 +536,5 @@ public class SysUserServiceImpl implements ISysUserService
             successMsg.insert(0, "恭喜您，数据已全部导入成功！共 " + successNum + " 条，数据如下：");
         }
         return successMsg.toString();
-    }
-    /**
-     * 通过电话号码查询用户
-     *
-     * @param phoneNumber 电话号码
-     * @return 用户对象信息
-     */
-    @Override
-    public SysUser selectUserByPhoneNumber(String phoneNumber)
-    {
-        return userMapper.selectUserByPhoneNumber(phoneNumber);
     }
 }
