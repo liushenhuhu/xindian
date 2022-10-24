85d0022b8b5647dace30ff11c13233cfde953748
hanhan <2668790143@qq.com>
2022-10-24 18:45:48 +0800

    pdf

--- ruoyi-xindian/src/main/java/com/ruoyi/xindian/download/controller/DownloadController.java
+++ ruoyi-xindian/src/main/java/com/ruoyi/xindian/download/controller/DownloadController.java
@@ -5,8 +5,12 @@ import com.ruoyi.common.core.controller.BaseController;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.*;
 
 
@@ -88,6 +92,58 @@ public class DownloadController extends BaseController {
             logger.error("错误信息：", e);
         } finally {
             os.close();
+        }
+    }
+
+    @RequestMapping(value = "uploadFile", method = RequestMethod.POST)
+    public String uploadFile(HttpServletRequest request, HttpServletRequest response, HttpSession session) {
+        MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+        MultipartFile multipartFile = multipartRequest.getFile("file1");//file是form-data中二进制字段对应的name
+
+        approvalFile(multipartFile);
+
+        System.out.println(multipartFile.getSize());
+        return "success";
+    }
+
+
+    public void approvalFile(MultipartFile file) {
+        OutputStream os = null;
+        InputStream inputStream = null;
+        String fileName = null;
+        try {
+            inputStream = file.getInputStream();
+            fileName = file.getOriginalFilename();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            String path = "F:\\test\\";
+            // 2、保存到临时文件
+            // 1K的数据缓冲
+            byte[] bs = new byte[1024];
+            // 读取到的数据长度
+            int len;
+            // 输出的文件流保存到本地文件
+            File tempFile = new File(path);
+            if (!tempFile.exists()) {
+                tempFile.mkdirs();
+            }
+            os = new FileOutputStream(tempFile.getPath() + File.separator + fileName);
+            // 开始读取
+            while ((len = inputStream.read(bs)) != -1) {
+                os.write(bs, 0, len);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // 完毕，关闭所有链接
+            try {
+                os.close();
+                inputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
