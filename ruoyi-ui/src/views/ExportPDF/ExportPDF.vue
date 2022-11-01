a6d3508cefbb6e1c0912cc892dbf596b01ead733
hanhan <2668790143@qq.com>
2022-11-01 19:54:46 +0800

    上传pdf按钮

--- ruoyi-ui/src/views/ExportPDF/ExportPDF.vue
+++ ruoyi-ui/src/views/ExportPDF/ExportPDF.vue
@@ -2801,57 +2801,8 @@ export default {
     //上传pdf
     upload_pdf(file) {
       // var url ='';
-
-      var _self = this
-      // 当下载pdf时，若不在页面顶部会造成PDF样式不对,所以先回到页面顶部再下载
-      let top = document.getElementById("pdfDom");
-      if (top != null) {
-        top.scrollIntoView();
-        top = null;
-      }
-      let title = this.exportPDFtitle;
-      html2Canvas(document.querySelector("#pdfDom"), {
-        //清晰度，越大越好
-        scale: 2,
-        allowTaint: true
-      }).then(function (canvas) {
-        // 获取canvas画布的宽高
-        let contentWidth = canvas.width;
-        let contentHeight = canvas.height;
-        // 一页pdf显示html页面生成的canvas高度;
-        let pageHeight = (contentWidth / 592.28) * 841.89;
-        // 未生成pdf的html页面高度
-        let leftHeight = contentHeight;
-        // 页面偏移
-        let position = 0;
-        // html页面生成的canvas在pdf中图片的宽高（本例为：横向a4纸[841.89,592.28]，纵向需调换尺寸）
-        let imgWidth = 592.28;
-        let imgHeight = (592.28 / contentWidth) * contentHeight;
-        let pageData = canvas.toDataURL("image/jpeg", 1.0);
-        let PDF = new JsPDF("", "pt", "a4");
-        // 两个高度需要区分: 一个是html页面的实际高度，和生成pdf的页面高度
-        // 当内容未超过pdf一页显示的范围，无需分页
-        if (leftHeight < pageHeight) {
-          PDF.addImage(pageData, "JPEG", 0, 0, imgWidth, imgHeight);
-        } else {
-          while (leftHeight > 0) {
-            PDF.addImage(pageData, "JPEG", 0, position, imgWidth, imgHeight);
-            leftHeight -= pageHeight;
-            position -= 841.89;
-            // 避免添加空白页
-            if (leftHeight > 0) {
-              PDF.addPage();
-            }
-          }
-        }
-        //PDF.save(title + ".pdf");
-        // 将pdf输入为base格式的字符串
-        var buffer = PDF.output("datauristring")
-        // 将base64格式的字符串转换为file文件
-        var myfile = _self.dataURLtoFile(buffer, title + ".pdf")
-        //name = _self.upload_pdf(myfile)
         var formdata = new FormData()
-        formdata.append("file", myfile); // 文件对象
+        formdata.append("file", file); // 文件对象
         console.log("上传pdf-1")
         //多个参数的情况
         // formdata.append("name", name);
@@ -2865,8 +2816,6 @@ export default {
             this.$modal.msgError("上传失败");
           }
         })
-
-      });
 
     },
     //将base64转换为文件对象
