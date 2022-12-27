6c03f216b94ac30d360141400612da730c7f673b
jielinmua <linghuima@163.com>
2022-12-27 20:56:08 +0800

    更新

--- ruoyi-ui/src/api/alert_log/alert_log.js
+++ ruoyi-ui/src/api/alert_log/alert_log.js
@@ -43,3 +43,12 @@ export function delAlert_log(logId) {
     method: 'delete'
   })
 }
+
+
+export function assigned_ano(data) {
+  return request({
+    url: '/alert_log/alert_log/assignedAno',
+    method: 'post',
+    data: data
+  })
+}
---
+++ ruoyi-ui/src/views/alert_log/alert_log/divide.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="divide">
+    <p>账号: {{ userId }}</p>
+    <el-input class="message" v-model="assignedAno.userId" placeholder="请输入账号" />
+    <br>
+    <span>患者id:</span>
+    <p style="white-space: pre-line;">{{ pId }}</p>
+
+    <el-input class="message" type="textarea" :rows="2" v-model="assignedAno.pId" placeholder="请输入患者id"></el-input>
+    <br>
+    <el-button class="sub" type="primary" @click=" submitMessage() ">确定</el-button>
+  </div>
+</template>
+<script>
+//调用接口
+// import { saveSubject } from "@/api/content";
+import {assigned_ano} from "@/api/alert_log/alert_log";
+
+export default {
+  name:"divide",
+  data() {
+    return {
+      assignedAno:{
+        userId: "",
+        pId:"",
+      }
+    };
+  },
+  methods: {
+    //添加增加题目
+    submitMessage() {
+      console.log(this.assignedAno.userId)
+      console.log(this.assignedAno.pId);
+      console.log(this.assignedAno)
+      assigned_ano()
+    },
+  },
+};
+</script>
+<style scoped>
+.divide{
+  margin: 0 auto;
+  width: 600px;
+}
+.message{
+  width: 300px;
+  margin-left: 30px;
+}
+.sub{
+  margin:30px 260px;
+}
+</style>
