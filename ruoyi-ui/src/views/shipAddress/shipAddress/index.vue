23ef65b45cea195bf6c111483fea45c8322a05b3
lxl <768398401@qq.com>
2023-07-04 17:03:18 +0800

    新增前端页面

---
+++ ruoyi-ui/src/views/shipAddress/shipAddress/index.vue
@@ -0,0 +1,343 @@
+<template>
+  <div class="app-container">
+    <el-form :model="queryParams" ref="queryForm" size="small" :inline="true" v-show="showSearch" label-width="68px">
+      <el-form-item label="用户电话" prop="patientPhone">
+        <el-input
+          v-model="queryParams.patientPhone"
+          placeholder="请输入用户电话"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="用户姓名" prop="patientName">
+        <el-input
+          v-model="queryParams.patientName"
+          placeholder="请输入用户姓名"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="街道地址" prop="streetAddress">
+        <el-input
+          v-model="queryParams.streetAddress"
+          placeholder="请输入街道地址"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="城市" prop="city">
+        <el-input
+          v-model="queryParams.city"
+          placeholder="请输入城市"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="省份" prop="state">
+        <el-input
+          v-model="queryParams.state"
+          placeholder="请输入省份"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="国家" prop="country">
+        <el-input
+          v-model="queryParams.country"
+          placeholder="请输入国家"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="邮政编码" prop="postalCode">
+        <el-input
+          v-model="queryParams.postalCode"
+          placeholder="请输入邮政编码"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="默认地址标识符" prop="defaultFlag">
+        <el-input
+          v-model="queryParams.defaultFlag"
+          placeholder="请输入默认地址标识符"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
+        <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
+      </el-form-item>
+    </el-form>
+
+    <el-row :gutter="10" class="mb8">
+      <el-col :span="1.5">
+        <el-button
+          type="primary"
+          plain
+          icon="el-icon-plus"
+          size="mini"
+          @click="handleAdd"
+          v-hasPermi="['system:shipAddress:add']"
+        >新增</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="success"
+          plain
+          icon="el-icon-edit"
+          size="mini"
+          :disabled="single"
+          @click="handleUpdate"
+          v-hasPermi="['system:shipAddress:edit']"
+        >修改</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="danger"
+          plain
+          icon="el-icon-delete"
+          size="mini"
+          :disabled="multiple"
+          @click="handleDelete"
+          v-hasPermi="['system:shipAddress:remove']"
+        >删除</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="warning"
+          plain
+          icon="el-icon-download"
+          size="mini"
+          @click="handleExport"
+          v-hasPermi="['system:shipAddress:export']"
+        >导出</el-button>
+      </el-col>
+      <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
+    </el-row>
+
+    <el-table v-loading="loading" :data="shipAddressList" @selection-change="handleSelectionChange">
+      <el-table-column type="selection" width="55" align="center" />
+      <el-table-column label="地址id" align="center" prop="addressId" />
+      <el-table-column label="用户电话" align="center" prop="patientPhone" />
+      <el-table-column label="用户姓名" align="center" prop="patientName" />
+      <el-table-column label="街道地址" align="center" prop="streetAddress" />
+      <el-table-column label="城市" align="center" prop="city" />
+      <el-table-column label="省份" align="center" prop="state" />
+      <el-table-column label="国家" align="center" prop="country" />
+      <el-table-column label="邮政编码" align="center" prop="postalCode" />
+      <el-table-column label="默认地址标识符" align="center" prop="defaultFlag" />
+      <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            type="text"
+            icon="el-icon-edit"
+            @click="handleUpdate(scope.row)"
+            v-hasPermi="['system:shipAddress:edit']"
+          >修改</el-button>
+          <el-button
+            size="mini"
+            type="text"
+            icon="el-icon-delete"
+            @click="handleDelete(scope.row)"
+            v-hasPermi="['system:shipAddress:remove']"
+          >删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <pagination
+      v-show="total>0"
+      :total="total"
+      :page.sync="queryParams.pageNum"
+      :limit.sync="queryParams.pageSize"
+      @pagination="getList"
+    />
+
+    <!-- 添加或修改地址管理对话框 -->
+    <el-dialog :title="title" :visible.sync="open" width="500px" append-to-body>
+      <el-form ref="form" :model="form" :rules="rules" label-width="80px">
+        <el-form-item label="用户电话" prop="patientPhone">
+          <el-input v-model="form.patientPhone" placeholder="请输入用户电话" />
+        </el-form-item>
+        <el-form-item label="用户姓名" prop="patientName">
+          <el-input v-model="form.patientName" placeholder="请输入用户姓名" />
+        </el-form-item>
+        <el-form-item label="街道地址" prop="streetAddress">
+          <el-input v-model="form.streetAddress" placeholder="请输入街道地址" />
+        </el-form-item>
+        <el-form-item label="城市" prop="city">
+          <el-input v-model="form.city" placeholder="请输入城市" />
+        </el-form-item>
+        <el-form-item label="省份" prop="state">
+          <el-input v-model="form.state" placeholder="请输入省份" />
+        </el-form-item>
+        <el-form-item label="国家" prop="country">
+          <el-input v-model="form.country" placeholder="请输入国家" />
+        </el-form-item>
+        <el-form-item label="邮政编码" prop="postalCode">
+          <el-input v-model="form.postalCode" placeholder="请输入邮政编码" />
+        </el-form-item>
+        <el-form-item label="默认地址标识符" prop="defaultFlag">
+          <el-input v-model="form.defaultFlag" placeholder="请输入默认地址标识符" />
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="submitForm">确 定</el-button>
+        <el-button @click="cancel">取 消</el-button>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import { listShipAddress, getShipAddress, delShipAddress, addShipAddress, updateShipAddress } from "@/api/shipAddress/shipAddress";
+
+export default {
+  name: "ShipAddress",
+  data() {
+    return {
+      // 遮罩层
+      loading: true,
+      // 选中数组
+      ids: [],
+      // 非单个禁用
+      single: true,
+      // 非多个禁用
+      multiple: true,
+      // 显示搜索条件
+      showSearch: true,
+      // 总条数
+      total: 0,
+      // 地址管理表格数据
+      shipAddressList: [],
+      // 弹出层标题
+      title: "",
+      // 是否显示弹出层
+      open: false,
+      // 查询参数
+      queryParams: {
+        pageNum: 1,
+        pageSize: 10,
+        patientPhone: null,
+        patientName: null,
+        streetAddress: null,
+        city: null,
+        state: null,
+        country: null,
+        postalCode: null,
+        defaultFlag: null
+      },
+      // 表单参数
+      form: {},
+      // 表单校验
+      rules: {
+      }
+    };
+  },
+  created() {
+    this.getList();
+  },
+  methods: {
+    /** 查询地址管理列表 */
+    getList() {
+      this.loading = true;
+      listShipAddress(this.queryParams).then(response => {
+        this.shipAddressList = response.rows;
+        this.total = response.total;
+        this.loading = false;
+      });
+    },
+    // 取消按钮
+    cancel() {
+      this.open = false;
+      this.reset();
+    },
+    // 表单重置
+    reset() {
+      this.form = {
+        addressId: null,
+        patientPhone: null,
+        patientName: null,
+        streetAddress: null,
+        city: null,
+        state: null,
+        country: null,
+        postalCode: null,
+        defaultFlag: null
+      };
+      this.resetForm("form");
+    },
+    /** 搜索按钮操作 */
+    handleQuery() {
+      this.queryParams.pageNum = 1;
+      this.getList();
+    },
+    /** 重置按钮操作 */
+    resetQuery() {
+      this.resetForm("queryForm");
+      this.handleQuery();
+    },
+    // 多选框选中数据
+    handleSelectionChange(selection) {
+      this.ids = selection.map(item => item.addressId)
+      this.single = selection.length!==1
+      this.multiple = !selection.length
+    },
+    /** 新增按钮操作 */
+    handleAdd() {
+      this.reset();
+      this.open = true;
+      this.title = "添加地址管理";
+    },
+    /** 修改按钮操作 */
+    handleUpdate(row) {
+      this.reset();
+      const addressId = row.addressId || this.ids
+      getShipAddress(addressId).then(response => {
+        this.form = response.data;
+        this.open = true;
+        this.title = "修改地址管理";
+      });
+    },
+    /** 提交按钮 */
+    submitForm() {
+      this.$refs["form"].validate(valid => {
+        if (valid) {
+          if (this.form.addressId != null) {
+            updateShipAddress(this.form).then(response => {
+              this.$modal.msgSuccess("修改成功");
+              this.open = false;
+              this.getList();
+            });
+          } else {
+            addShipAddress(this.form).then(response => {
+              this.$modal.msgSuccess("新增成功");
+              this.open = false;
+              this.getList();
+            });
+          }
+        }
+      });
+    },
+    /** 删除按钮操作 */
+    handleDelete(row) {
+      const addressIds = row.addressId || this.ids;
+      this.$modal.confirm('是否确认删除地址管理编号为"' + addressIds + '"的数据项？').then(function() {
+        return delShipAddress(addressIds);
+      }).then(() => {
+        this.getList();
+        this.$modal.msgSuccess("删除成功");
+      }).catch(() => {});
+    },
+    /** 导出按钮操作 */
+    handleExport() {
+      this.download('system/shipAddress/export', {
+        ...this.queryParams
+      }, `shipAddress_${new Date().getTime()}.xlsx`)
+    }
+  }
+};
+</script>
