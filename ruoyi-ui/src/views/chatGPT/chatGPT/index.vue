45da5bc97950aa591a7d17448ed795bba27fddb4
lxl <768398401@qq.com>
2023-07-29 17:28:56 +0800

    调整机器人界面

--- ruoyi-ui/src/views/chatGPT/chatGPT/index.vue
+++ ruoyi-ui/src/views/chatGPT/chatGPT/index.vue
@@ -3,51 +3,61 @@
     <div class="mainbox">
       <div class="box">
         <div class="title">
-          <img src="@/assets/images/logo1.png" style="margin-top: 1%" width="4%" height="50%" alt />
-          <img src="@/assets/images/font.png" width="13%" height="50%" />
+          <img src="@/assets/images/chatGPT-logo.png" width="" height="84%">
+          <img src="@/assets/images/chatGPT-logo-font.png" width="" height="84%">
+          <img src="@/assets/images/chatGPT-logo2.png" width="" height="40%">
         </div>
+
         <div id="content" class="content">
-          <div v-for="(item,index) in info" :key="index">
-            <div class="info_r info_default" v-if="item.type == 'leftinfo'">
+        <!--    left      -->
+          <div class="left">
+            <div class="left-child">
+              <textarea
+                placeholder="请输入您的问题..."
+                style="height: 100%;width: 100%;resize:none;outline: none;border-color:#ccc;border-radius:5px;font-size: 24px"
+                id="text"
+                v-model="customerText"
+                @keyup.enter="sentMsg()"
+              ></textarea>
+            </div>
+            <div class="left-btn">
+              <button @click="sentMsg()" style="width: 60%;height: 60%;background-color: rgba(68,114,196);">
+                <span style="vertical-align: 3px;font-size: 26px; color: white">发 送</span>
+              </button>
+            </div>
+          </div>
+        <!--    right      -->
+          <div class="right" id="right">
+            <div v-for="(item,index) in info" :key="index">
+              <div class="info_r info_default" v-if="item.type == 'leftinfo'">
               <span class="circle circle_r">
                 <img src="@/assets/images/robot.gif" width="40px" height="40px" style="margin-left: 1px" alt/>
               </span>
-              <div class="con_r con_text">
-                <div>{{item.content}}</div>
-                <div v-for="(item2,index) in item.question" :key="index">
-                  <div class="con_que" @click="clickRobot(item2.content,item2.id)">
-                    <div class="czkj-question-msg">
-                      {{item2.index}}
-                      {{item2.content}}
+                <div class="con_r con_text">
+                  <div>{{item.content}}</div>
+                  <div v-for="(item2,index) in item.question" :key="index">
+                    <div class="con_que" @click="clickRobot(item2.content,item2.id)">
+                      <div class="czkj-question-msg">
+                        {{item2.index}}
+                        {{item2.content}}
+                      </div>
                     </div>
                   </div>
                 </div>
+                <div class="time_r">{{item.time}}</div>
               </div>
-              <div class="time_r">{{item.time}}</div>
-            </div>
-
-            <div class="info_l" v-if="item.type == 'rightinfo'">
-              <div class="con_r con_text">
-                <span class="con_l">{{item.content}}</span>
-                <span class="circle circle_l">
-                  <img src class="pic_l" />
+              <div class="info_l" v-if="item.type == 'rightinfo'">
+                <div class="con_r con_text">
+                  <span class="con_l">{{item.content}}</span>
+                  <span class="circle circle_l">
+                  <img src="@/assets/images/logo1.png" class="pic_l" />
                 </span>
+                </div>
+                <div class="time_l">{{item.time}}</div>
               </div>
-              <div class="time_l">{{item.time}}</div>
             </div>
           </div>
-        </div>
-        <div class="setproblem">
-          <textarea
-            placeholder="请输入您的问题..."
-            style="height: 8vh;width: 100%;resize:none;outline: none;border-color:#ccc;border-radius:5px;font-size: 20px"
-            id="text" maxlength="150"
-            v-model="customerText"
-            @keyup.enter="sentMsg()"
-          ></textarea>
-          <button @click="sentMsg()" style="display: block;margin-top: -0.25%;margin-right: -4%">
-            <span style="vertical-align: 3px;">发 送</span>
-          </button>
+
         </div>
       </div>
     </div>
@@ -98,6 +108,7 @@ export default {
           content: text,
         };
         this.info.push(obj);
+        this.customerText = "";
         proxyRequest(this.queryParams).then(response => {
           //console.log(response.response);
           this.customerText = "";
@@ -110,7 +121,7 @@ export default {
           this.appendRobotMsg(response.response);
         })
         this.$nextTick(() => {
-          var contentHeight = document.getElementById("content");
+          var contentHeight = document.getElementById("right");
           contentHeight.scrollTop = contentHeight.scrollHeight;
         });
       }
@@ -124,7 +135,7 @@ export default {
       let answerText = "";
       let flag;
       for (let i = 0; i < this.robotAnswer.length; i++) {
-        console.log(this.robotAnswer[i].content)
+        //console.log(this.robotAnswer[i].content)
         if (this.robotAnswer[i].content.indexOf(text) != -1) {
           flag = true;
           answerText = this.robotAnswer[i].content;
@@ -140,7 +151,7 @@ export default {
           question: [],
         };
         this.info.push(obj);
-      } else {
+      } /*else {
         answerText = "您可能想问：";
         let obj = {
           type: "leftinfo",
@@ -150,9 +161,9 @@ export default {
           question: this.robotQuestion,
         };
         this.info.push(obj);
-      }
+      }*/
       this.$nextTick(() => {
-        var contentHeight = document.getElementById("content");
+        var contentHeight = document.getElementById("right");
         contentHeight.scrollTop = contentHeight.scrollHeight;
       });
     },
@@ -180,7 +191,7 @@ export default {
       this.info.push(obj_r);
       this.info.push(obj_l);
       this.$nextTick(() => {
-        var contentHeight = document.getElementById("content");
+        var contentHeight = document.getElementById("right");
         contentHeight.scrollTop = contentHeight.scrollHeight;
       });
     },
@@ -198,13 +209,13 @@ export default {
       };
       this.info.push(happyEnding);
       this.$nextTick(() => {
-        var contentHeight = document.getElementById("content");
+        var contentHeight = document.getElementById("right");
         contentHeight.scrollTop = contentHeight.scrollHeight;
       });
 
     },
     showTimer() {
-      this.timer = setTimeout(this.endMsg, 1000*60);
+      this.timer = setTimeout(this.endMsg, 10000*60);
     },
     getTodayTime() {
       // 获取当前时间
@@ -256,16 +267,22 @@ export default {
     height: calc( 100vh - 84px);
     background-color: #ffffff;
     position: relative;
-    background-image: url("../../../assets/images/body.png");
+    /*background-image: url("../../../assets/images/body.png");
     background-repeat:no-repeat;
     background-size:100% 110%;
-    background-position: center;
+    background-position: center;*/
     z-index: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
     #content {
-      height: calc(100% - 20vh);
-      overflow-y: scroll;
+      height: calc(100% - 16vh);
+      //overflow-y: scroll;
       font-size: 20px;
-      width: 100%;
+      width: 98%;
+      display: flex;
+      justify-content: flex-start;
       .circle {
         display: inline-block;
         width: 42px;
@@ -277,6 +294,8 @@ export default {
       .con_text {
         color: #333;
         margin-bottom: 5px;
+        display: flex;
+        justify-content: flex-end;
       }
       .con_que {
         color: #1c88ff;
@@ -314,11 +333,10 @@ export default {
         margin-right: 35px;
         color: #ffffff;
         color: #3163C5;
-        margin-top: 10px;
         .pic_l {
-          width: 17px;
-          height: 17px;
-          margin: 9px;
+          width: 40px;
+          height: 40px;
+          margin-top: 6px;
           //margin-right: 20px;
         }
         .time_l {
@@ -337,8 +355,8 @@ export default {
           text-align: left;
           color: #fff;
           margin-right: 5px;
-          /*word-wrap:break-word;
-          word-break:break-all;*/
+          word-wrap:break-word;
+          word-break:break-all;
           //overflow: hidden;/*这个参数根据需求来决定要不要*/
         }
         .circle_l {
@@ -404,21 +422,19 @@ export default {
   color: #3163C5;
 }
 .title {
-  width: 100%;
+  width: 98%;
   //position: absolute;
   margin: 0 auto !important;
-  text-align: center;
+  //line-height: 2%;
+  //text-align: center;
   //left: 45%;
   height: 12vh;
   font-size: 32px;
-  background: url("../../../assets/images/title.png");
-  background-repeat:no-repeat;
-  background-size:100% 100%;
-  background-position: center;
-  background-color:rgb(3,4,74);
+  border: 4px rgba(47,82,143) solid;
+  display: flex;
+  align-items: center;
   img {
-    display: inline;
-    margin-top: 0.5%;
+  margin: 0 30px;
   }
 }
 .title-hn {
@@ -426,6 +442,33 @@ export default {
   //vertical-align: middle;
   //float: right;
   //margin-right: 46%;
+}
+.left {
+  width: 30%;
+  border: 3px rgba(47,82,143) solid;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+}
+.left-child {
+  width: 90%;
+  height: 80%;
+  border: 2px rgba(47,82,143) solid;
+  margin-top: 5%;
+}
+.right {
+  width: 70%;
+  border: 3px rgba(47,82,143) solid;
+  overflow-y: scroll;
+}
+
+.left-btn {
+  width: 100%;
+  height: 10%;
+  //border: 2px pink solid;
+  display: flex;
+  justify-content: space-around;
 }
 
 </style>
