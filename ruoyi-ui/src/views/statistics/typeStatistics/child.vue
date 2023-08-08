588bb2226d5086ea3b095cbf9c3497e497aec4ee
lxl <768398401@qq.com>
2023-08-08 09:24:35 +0800

    新增子页面，调试

---
+++ ruoyi-ui/src/views/statistics/typeStatistics/child.vue
@@ -0,0 +1,102 @@
+<template>
+        <div id="myChart1" style="width: 25%;height: 35vh; margin-top: 20%"> </div>
+</template>
+<script>
+import { listTypeStatistics } from "@/api/statistics/typeStatistics";
+export default {
+  name: 'hello',
+  data () {
+    return {
+      showSearch: true,
+      statistics: [],
+      options: [],
+      arr: [],
+      xArr: [],
+      yArr: [],
+      show: true,
+      // 总条数
+      total: 0,
+      queryParams: {
+        pageNum: 1,
+        pageSize: 10,
+        doctorName: null,
+        doctorPhone: null,
+        month: null,
+        reportType:null,
+      },
+    }
+  },
+  created() {
+    this.getList();
+  },
+  mounted(){
+    this.drawLine1();
+  },
+  methods: {
+    drawLine1() {
+      // 基于准备好的dom，初始化echarts实例
+      let myChart = this.$echarts.init(document.getElementById('myChart1'))
+      // 绘制图表
+      let status = this;
+      myChart.off('click');
+      myChart.setOption({
+        title: {
+          text: '心电类型统计图'
+        },
+        tooltip: {},
+        xAxis: {
+          data: this.xArr,
+          axisLabel:{
+            interval: 0,
+            rotate : 50,
+          }
+        },
+        yAxis: {},
+        series: [{
+          //name: '数量',
+          type: 'bar',
+          data: this.yArr
+        }],
+        grid: {
+          bottom: '20%',
+          top: '0%',
+          right: '10%',
+          left: '0%',
+          //height: '100%'
+        },
+      });
+    },
+    /** 查询 */
+    getList() {
+      this.loading = true;
+      listTypeStatistics(this.queryParams).then(response => {
+        //console.log(response);
+        this.arr = response;
+        this.arr.forEach(item => {
+          if(item.reportType === 'JECGsingleGZ' || item.reportType === 'JECG12YXD' || item.reportType === 'JECGsingleWL' || item.reportType === 'JECG12SX'){
+            this.xArr.push(item.reportType)
+            this.yArr.push(item.count);
+          }
+        })
+        this.drawLine1();
+        /*this.drawLine2();
+        this.drawLine3();
+        this.drawLine4();
+        this.loading = false;*/
+      });
+    },
+    /** 搜索按钮操作 */
+    handleQuery() {
+      this.queryParams.pageNum = 1;
+      this.getList();
+    },
+    backQuery(){
+      this.show = true;
+      document.getElementById("myChart").style.display='';
+      document.getElementById("table1").style.display='none';
+    }
+  },
+}
+</script>
+<style>
+</style>
