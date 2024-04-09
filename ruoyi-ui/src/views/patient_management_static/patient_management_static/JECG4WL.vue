4822cdd2d9aaa6cbc38b6e4f6376622316e13b7b
wanf <wjh011126@163.com>
2024-04-09 11:39:38 +0800

    解决静态四导没有缓存问题

--- ruoyi-ui/src/views/patient_management_static/patient_management_static/JECG4WL.vue
+++ ruoyi-ui/src/views/patient_management_static/patient_management_static/JECG4WL.vue
@@ -521,7 +521,7 @@ import {getVerify} from "@/api/verify/verify";
 import {docList} from "@/api/doctor/doctor";
 
 export default {
-  name: "JECG4WL",
+  name: "All_JECG4",
   provide () {    //父组件中通过provide来提供变量，在子组件中通过inject来注入变量。
     return {
       reload: this.reload
@@ -618,14 +618,14 @@ export default {
   beforeCreate() {
     // updateOnlineAll();
   },
-  activated() {
-    document.documentElement.scrollTop=this.currentScrollPos || 0
-  },
-
-  beforeRouteLeave(to,from,next) {
-    this.currentScrollPos = document.documentElement.scrollTop || 0
-    next()
-  },
+  // activated() {
+  //   document.documentElement.scrollTop=this.currentScrollPos || 0
+  // },
+  //
+  // beforeRouteLeave(to,from,next) {
+  //   this.currentScrollPos = document.documentElement.scrollTop || 0
+  //   next()
+  // },
 
   created() {
     getEcgType(this.ecgType).then(r=>{
