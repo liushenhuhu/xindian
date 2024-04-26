1b3507f25605b98bf2af35705c599d6d254e0899
wanf <wjh011126@163.com>
2024-04-26 09:33:54 +0800

    放开30分钟诊断换人

--- ruoyi-xindian/src/main/java/com/ruoyi/xindian/order/listenner/RedisKeyExpirationListener.java
+++ ruoyi-xindian/src/main/java/com/ruoyi/xindian/order/listenner/RedisKeyExpirationListener.java
@@ -141,25 +141,25 @@ public class RedisKeyExpirationListener extends KeyExpirationEventMessageListene
                 }
             }
 
-//            //用于提交心电，加入抢单，十分钟过期，判断有没有诊断
-//            if (split[0].equals("reportPT")){
-//                try {
-//                   wxMsgRunConfig.redisTOrAdd(split[1]);
-//                } catch (Exception e) {
-//                    System.out.println(e);
-//                }
-//                return;
-//            }
-//
-//            //用于医生诊断，判断诊断30分钟内是否
-//            if (split[0].equals("reportDT")){
-//                try {
-//                    wxMsgRunConfig.redisDTTime(split[1]);
-//                } catch (Exception e) {
-//                    System.out.println(e);
-//                }
-//                return;
-//            }
+            //用于提交心电，加入抢单，十分钟过期，判断有没有诊断
+            if (split[0].equals("reportPT")){
+                try {
+                   wxMsgRunConfig.redisTOrAdd(split[1]);
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+                return;
+            }
+
+            //用于医生诊断，判断诊断30分钟内是否
+            if (split[0].equals("reportDT")){
+                try {
+                    wxMsgRunConfig.redisDTTime(split[1]);
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+                return;
+            }
             if (expiredKey.equals("ecgCountType")){
                 ecgCountController.getTypeList();
             }
